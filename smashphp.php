c7798b316ff803fd870cf05c033d97aa38069c52
HorseScary <jakobaayers@gmial.com>
2021-10-18 13:31:48 -0400

    added the php

---
+++ smashphp.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+        <title>Keysmash</title>
+</head>
+<body>
+        <h2> owo </h2>
+<?php
+        $letters = $_POST['letters'];
+        $length = $_POST['length'];
+        $case = $_POST['case'];
+
+        echo 'letters: ' . $letters . '<br \>';
+        echo 'length: ' . $length . ' <br \>';
+        echo 'case: ' . $case . '<br \>';
+?>
+
+
+</body>
+<html>
