7ee24e61286955b704b73d73c493e561a2f49890
HorseScary <jakobaayers@gmial.com>
2021-11-08 08:56:12 -0500

    made (basic) generator w javascript

---
+++ smash.js
@@ -0,0 +1,20 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1) + min);
+}
+
+function smash() {
+    var smashLetters =  document.myform.letters.value;
+    var smashLength = document.myform.length.value;
+    var smashCase = document.myform.case.value;
+    var smash = '';
+
+    var letterLength = smashLetters.length;
+    for (i=0; i<parseInt(smashLength); i++) {
+        smash += smashLetters.charAt(getRandomIntInclusive(0, letterLength))
+    }
+
+    document.write(smash)
+//    document.write(letters + "<br>" + length + "<br>" + smashCase)
+}
